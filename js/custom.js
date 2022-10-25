8f8b534e36c5fe7afaf83cf97f8c23ef5ea140d0
swagata-codearts <swagata.codearts@gmail.com>
2022-10-25 13:50:13 +0530

    js: gallery after before

--- js/custom.js
+++ js/custom.js
@@ -15,3 +15,21 @@ jQuery('#custom-owl').owlCarousel({
         }
     }
 });
+
+
+jQuery( ".gallery-a-btn" ).each(function() {
+    jQuery(this).on("click", function(){
+        jQuery(this).parent().toggleClass('cus-hide')
+        jQuery(this).parent().siblings('.gal-img-btn-yn').toggleClass('cus-hide')
+        jQuery(this).parent().parent('.gallery-img-box').css('background-color', "rgb(205, 172, 130)")
+    });
+});
+jQuery('.gal-img-button-n').click(function(){
+    jQuery(this).parent().parent().find('.gal-img-btn').removeClass('cus-hide')
+    jQuery(this).parent().parent().find('.gal-img-btn-yn').addClass('cus-hide')
+    jQuery(this).parent().parent('.gallery-img-box').css('background-color', "#fff")
+});
+jQuery('.gal-img-button-y').click(function(){
+    jQuery(this).parent().parent().siblings('.gallery-image ').removeClass('cus-hide')
+    jQuery(this).parent().parent('.gallery-img-box').addClass('cus-hide')
+});
